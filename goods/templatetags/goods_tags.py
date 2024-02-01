67028e806121bb525d2a7bc7d3354c04f254511c
DenisGST <stationgrad@gmail.com>
2024-02-01 17:05:49 +0300

    tag for categories

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -2,9 +2,9 @@ from django import template
 
 from goods.models import Categories
 
+
 register = template.Library()
 
 @register.simple_tag()
 def tag_categories():
-    
-    return Categories.object.all()
+    return Categories.objects.all()
