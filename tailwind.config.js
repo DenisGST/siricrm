7c514ac1481be8d478dc4f6269f7f798f7f6f046
Denis Kanygin <grad_station@mail.ru>
2026-03-27 10:24:36 +0000

    Store 27/03/2026

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,35 @@ module.exports = {
   content: [
     "./templates/**/*.html",
     "./**/templates/**/*.html",
-    "./**/*.js",
+    "./static/js/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+  themes: [
+    {
+      siricrm: {
+        "primary":          "#0079d3",   // синие кнопки
+        "primary-content":  "#ffffff",   // текст на синих кнопках
+        "secondary":        "#6890b9",   // акцент
+        "accent":           "#0545ac",
+        "neutral":          "#1a1a1b",
+        "base-100":         "#ffffff",   // фон карточек
+        "base-200":         "#f6f7f8",   // фон сайдбара
+        "base-300":         "#edeff1",   // границы
+        "base-content":     "#1a1a1b",   // основной текст
+        "info":             "#e9f5ff",
+        "success":          "#36d399",
+        "warning":          "#fbbd23",
+        "error":            "#f87171",
+      },
+    },
+  ],
+},
 };
